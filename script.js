750eaecf2b2d0e867e9499df1bdef540510314f5
Lívia Rocha <liviaantenucci@colegiopaulodetarso.com.br>
2026-03-17 14:47:49 +0000

    salvo 1

---
+++ script.js
@@ -0,0 +1,7 @@
+const taskBtn = document.querySelector("#taskBtn") // Variável do Botão
+const taskList = document.querySelector("#taskList") // Variável da Lista
+const taskInput = document.querySelector("#taskInput") // Variável do Campo de Texto (Input)
+
+taskBtn.addEventListener("click", function(){
+    alert("CLIQUEI!!!")
+})

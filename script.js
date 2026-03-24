ff8cf7d1dc67aedb52018e58c3bc85b9c7971538
Lívia Rocha <liviaantenucci@colegiopaulodetarso.com.br>
2026-03-24 14:48:53 +0000

    salvo 2

--- script.js
+++ script.js
@@ -3,5 +3,22 @@ const taskList = document.querySelector("#taskList") // Variável da Lista
 const taskInput = document.querySelector("#taskInput") // Variável do Campo de Texto (Input)
 
 taskBtn.addEventListener("click", function(){
-    alert("CLIQUEI!!!")
+    const li = document.createElement('li');
+    const taskInputValue = taskInput.value;
+    if (taskInputValue == ''){
+        return
+    };
+    const span = document.createElement('span');
+    span.textContent = taskInputValue;
+    const button = document.createElement('button');
+    button.textContent = 'Excluir';
+    button.addEventListener('click', function(){
+        li.remove();'0o'
+    })
+    
+    li.appendChild(span)
+    li.appendChild(button)
+
+    li.textContent = taskInputValue;
+    taskList.appendChild(li);
 })

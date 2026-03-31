8512584aa23e7de3e1a15828819661ee6bec03ab
Lívia Rocha <liviaantenucci@colegiopaulodetarso.com.br>
2026-03-31 14:29:57 +0000

    js

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
-const taskBtn = document.querySelector("#taskBtn") // Variável do Botão
-const taskList = document.querySelector("#taskList") // Variável da Lista
-const taskInput = document.querySelector("#taskInput") // Variável do Campo de Texto (Input)
+const taskBtn = document.querySelector("#taskBtn") // Seleciona o Botão Adicionar Tarefa;
+const taskList = document.querySelector("#taskList") // Seleciona a Lista de Tarefas;
+const taskInput = document.querySelector("#taskInput") // Seleciona o Input de Tarefas;
 
-taskBtn.addEventListener("click", function(){
-    const li = document.createElement('li');
-    const taskInputValue = taskInput.value;
-    if (taskInputValue == ''){
-        return
-    };
+
+function createSpan(taskInputValue){ // Função que cria automaticamente spans de forma separada;
     const span = document.createElement('span');
     span.textContent = taskInputValue;
-    const button = document.createElement('button');
-    button.textContent = 'Excluir';
-    button.addEventListener('click', function(){
-        li.remove();'0o'
-    })
-    
-    li.appendChild(span)
-    li.appendChild(button)
+    return span;
+}
 
-    li.textContent = taskInputValue;
+function createDeleteBtn(li){
+    const button = document.createElement('button');
+    button.textContent = 'Excluir'
+    button.addEventListener('click', function(){
+        li.remove();
+    });
+    return button;
+}
+
+function addTask(){ // Função que adiciona um item na lista, junto do seu botão de remover;
+    const li = document.createElement('li');
+    const taskInputValue = taskInput.value; 
+    if (taskInputValue == ''){
+        return
+    }
+    const span = createSpan(taskInputValue);
+    const button = createDeleteBtn(li);
+
+    li.appendChild(span);
+    li.appendChild(button);
+
     taskList.appendChild(li);
-})
+}
+
+
+taskBtn.addEventListener("click", addTask) //Função que faz com que o click no Adicionar Tarefa adicione uma tarefa;
+    
